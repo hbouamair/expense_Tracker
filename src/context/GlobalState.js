07a9dd82deb109d5a799a086ac27ea85a0cd922c
Hamza Bouamair <Hamza.bouamair@gmail.com>
2021-07-22 22:04:30 +0100

    new commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -15,11 +15,32 @@ const initialState = {
 export const GlobalContext = createContext(initialState);
 
 
+
+
 //Provider Component
 export const GlobalProvider = ({children}) => { 
     const [state , dispatch] = useReducer(AppReducer , initialState);
+
+        //Actions 
+        function deleteTransaction(id){
+            dispatch({
+                type : 'DELETE_TRANSACTION', 
+                payload : id
+            }) ;
+
+        }
+        function addTransaction(transaction){
+            dispatch({
+                type : 'ADD_TRANSACTION', 
+                payload : transaction
+            }) ;
+
+        }
+
+
      return (<GlobalContext.Provider  value={{
-         transactions: state.transactions
+         transactions: state.transactions,
+          deleteTransaction , addTransaction
      }}>
              {children}
         </GlobalContext.Provider>)
